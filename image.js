18743056a78db0386d0a437082fe6fed6d709b04
mihalcinmarek <41336140+mihalcinmarek@users.noreply.github.com>
2019-02-16 11:18:36 +0100

    Add files via upload

---
+++ image.js
@@ -0,0 +1,30 @@
+const Clarifai = require ('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '678d75d580ad4df6899008744e8c7e6d'
+   });
+
+const handleApiCall = (req, res) => {
+   app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then (data => {
+        res.json (data);
+    })
+    .catch (err => res.status (400).json ('unable to work with API'))
+}
+
+const handleImage = (req,res) => {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment ('entries', 1)
+    .returning ('entries')
+    .then (entries => {
+        res.jeson(entries[0]);
+    })
+    .catch(err => res.status (400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
